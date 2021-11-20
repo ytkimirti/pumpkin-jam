0e5f69d181c09561d155bfaf9ce9dd82572e825c
enesvan <enesvan206@gmail.com>
2021-11-20 16:45:45 +0300

    interactable script

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    private bool rangeCheck;
+
+    public UnityEvent Interact;
+
+    private void Start()
+    {
+        rangeCheck = false;
+    }
+
+    private void Update()
+    {
+        if (rangeCheck)
+        {
+            if (Input.GetKeyDown(KeyCode.E)) Interact.Invoke();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player") rangeCheck = true;
+        else rangeCheck = false;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        rangeCheck = false;
+    }
+}
